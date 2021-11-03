6a270b40378c99649d7ba418b215e52f3e2f05ea
Discoonect <67423146+Discoonect@users.noreply.github.com>
2021-11-03 12:02:36 +0900

    codepush

---
+++ App.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import codePush from 'react-native-code-push';
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!  codepush update</Text>
+    </View>
+  );
+};
+
+const codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_START,
+  // 언제 업데이트를 체크하고 반영할지를 정한다.
+  // ON_APP_RESUME은 Background에서 Foreground로 오는 것을 의미
+  // ON_APP_START은 앱이 실행되는(켜지는) 순간을 의미
+  updateDialog: false,
+  // 업데이트를 할지 안할지 여부에 대한 노출 (잠수함 패치의 경우 false)
+  installMode: codePush.InstallMode.IMMEDIATE
+  // 업데이트를 어떻게 설치할 것인지 (IMMEDIATE는 강제설치를 의미)
+};
+
+const styles = StyleSheet.create({
+  container : {
+    justifyContent : 'center'
+  }
+});
+
+export default codePush(codePushOptions)(App);
